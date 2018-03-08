e2aa9529c3709cdd18eadcae965cff94dc715a89
Lennart von Werder <lennartvonwerder@gmail.com>
2018-03-08 15:50:00 +0100

    added login page

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+
+
+	<meta http-equiv="refresh" content="1; URL=views/login.php">
+
+
+</head>
+<body>
+Weiterleitung zum Login
+</body>
+
+</html>
+
---
+++ views/login.php
@@ -0,0 +1,36 @@
+<?php
+
+include('../functions.php');
+
+include('../parts/top.php');
+
+?>
+
+<div class="content">
+
+<h1>ORGNZD</h1>
+
+<h3>Login</h3>
+
+
+<form action="" method="post">
+
+<input type="text" name="username">
+
+<input type="password" name="password">
+
+<input type="Submit" name="submit" value="Login">
+
+
+</form>
+
+
+
+
+</div>
+
+<?php
+
+include('../parts/bottom.php')
+
+?>
