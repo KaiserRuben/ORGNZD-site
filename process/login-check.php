34c63485754dccee1fb17d52e90c1ebda90b1519
Lennart von Werder <lennartvonwerder@gmail.com>
2018-03-08 16:20:05 +0100

    next

--- process/login-check.php
+++ process/login-check.php
@@ -3,11 +3,12 @@
 include('../functions.php');
 
 if(isset($_POST['email']) && isset($_POST['password']) && $_POST['email'] !== '' && $_POST['password'] !== ''){
+	
 	$email = $_POST['email'];
 	$password = $_POST['password'];
 
 	login($email, $password);
-
+		
 }else{
 	echo 'Fehler';
 }
