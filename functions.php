5b3a30be3a19e4774ea7802080c813d9072f4ec2
Ruben Kaiser <C:\Users\Ruben\AppData\Roaming\The Bat!>
2018-03-08 16:19:24 +0100

    login function done (session missing)

--- functions.php
+++ functions.php
@@ -9,5 +9,14 @@
 
 function login($email, $password)
 {
-	echo 'Login daten erhalten';
+  $sql = "SELECT password FROM user WHERE id={$email}";
+  $pdo->query($sql) as $row;
+  if($row['password'] = $password){
+    //Hier fehlt noch die Session
+    print('Hat geklappt');
+    return 1;
+  }
+  else{
+    return 0;
+  }
 }
